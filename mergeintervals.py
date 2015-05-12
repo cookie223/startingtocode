14021fa81b0e2cdb6c11f49f6c30892ccc46fa03
Qi Chu <g.cookie223@gmail.com>
2015-05-12 16:40:44 +0800

    review

---
+++ mergeintervals.py
@@ -0,0 +1,16 @@
+class Interval:
+    def __init__(self, s = 0, e = 0):
+        self.start = s
+        self.end  = e
+
+
+class Solution:
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x.start)
+        res = []
+        for i in intervals:
+            if res and i.start <= res[-1].end:
+                res[-1].end = max(i.end, res[-1].end)
+            else:
+                res.append(i)
+        return res
